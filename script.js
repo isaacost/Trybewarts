9a5899e44894ff16c286bff7cdc10bfd52308fb4
Lais Omena <laisf.omena@gmail.com>
2022-08-03 15:54:22 +0100

    Requisito 21 :rocket:

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const senha = document.getElementById('senha');
 const input = document.getElementById('agreement');
 const button = document.getElementById('submit-btn');
 const textarea = document.getElementById('textarea');
+const feedbackArea = document.getElementById('feedback-area');
+const formDataArea = document.getElementById('form-data');
+const formPageArea = document.getElementById('main-form');
 
 function validation() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -27,29 +30,58 @@ function contador() {
   document.getElementById('counter').innerHTML = counter;
 }
 
-input.addEventListener('change', disabledButton);
-btnEntrar.addEventListener('click', validation);
-textarea.addEventListener('keyup', contador);
+function createElement(question, answer) {
+  const element = document.createElement('li');
+  element.innerHTML = `<strong>${question}:</strong> 
+  <span>${answer}</span>`;
+  return element;
+}
 
+function deleteForm() {
+  feedbackArea.classList.remove('hidden');
+  formPageArea.classList.add('hidden');
+}
+
+function newPage(formData) {
+  deleteForm();
+  for (let i = 0; i < formData.length; i += 1) {
+    const { question, answer } = formData[i];
+    const newElement = createElement(question, answer);
+    formDataArea.appendChild(newElement);
+  }
+}
 const name = document.getElementById('input-name');
 const lastName = document.getElementById('input-lastname');
 const emailForm = document.getElementById('input-email');
 const house = document.getElementById('house');
-const family = document.querySelector('input[name = "family"]:checked');
-const content = document.querySelector('input[name = "content"]:checked');
-const avaluation = document.querySelector('input[name = "rate"]:checked');
-
-function getData() {
-  const objeto = [
-    { question: 'Nome completo', answer: `${name.value} ${lastName.value}` },
-    { question: 'E-mail', answer: emailForm.value },
-    { question: 'Casa', answer: house.value },
-    { question: 'Família', answer: family.innerText },
-    { question: 'Quer aprender:', answer: content.innerText },
-    { question: 'Avaliação', answer: avaluation.innerText },
-  ];
-
-  console.log(objeto);
+function getContent() {
+  const content = document.querySelectorAll('input[name = "content"]:checked');
+  const arr = [];
+  for (let i = 0; i < content.length; i += 1) {
+    arr.push(content[i].value);
+  }
+  return arr.join(', ');
 }
 
+function getData(e) {
+  e.preventDefault();
+  const text = document.getElementById('textarea');
+  const family = document.querySelector('input[name = "family"]:checked').value;
+  const content = getContent();
+  const avaluation = document.querySelector('input[name = "rate"]:checked').value;
+  const objeto = [
+    { question: 'Nome', answer: `${name.value} ${lastName.value}` },
+    { question: 'Email', answer: emailForm.value },
+    { question: 'Casa', answer: house.value },
+    { question: 'Família', answer: family },
+    { question: 'Matérias', answer: content },
+    { question: 'Avaliação', answer: avaluation },
+    { question: 'Observações', answer: text.value },
+  ];
+  return newPage(objeto);
+}
+
+input.addEventListener('change', disabledButton);
+btnEntrar.addEventListener('click', validation);
+textarea.addEventListener('keyup', contador);
 button.addEventListener('click', getData);
